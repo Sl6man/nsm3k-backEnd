4fed23c382f911c2dee287a74039b93ac92f87e4
Sultan Alanbari <sultanalanbari@Sultans-MacBook-Air.local>
2022-09-23 23:27:57 +0300

    last

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require('http')
+const socketIo = require('socket.io')
+const app = express()
+const server = http.createServer(app)
+const cors = require('cors')
+
+app.use(cors())
+
+const io = socketIo(server, {
+    cors:{
+        origin: "*"
+    }
+}
+)
+
+
+io.on('connection', socket=>{
+    // console.log('user has joined: ' + socket.id)
+    
+    socket.on('join-room', (room)=>{
+        socket.join(room)
+    })
+
+    socket.on('sendMessage', data=>{
+        socket.broadcast.to(data.room).emit('sendMessage', {msg:data.msg, isMe:false, imgSrc:data.imgSrc})
+    })
+
+})
+
+const PORT = 4000 || process.env.PORT
+
+server.listen(PORT, ()=>{
+    console.log("server runing")
+})
